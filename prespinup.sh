4d45c026f921bceae56e0e8d1b8ebe1156cb4dd5
Andy Aschwanden <aaschwanden@alaska.edu>
2013-10-09 09:46:51 +0200

    - updated prespinup script

--- prespinup.sh
+++ prespinup.sh
@@ -9,8 +9,14 @@ My=339
 Mx=86
 My=169
 
-Mz=100
+Mx=86
+My=169
+
+Mx=45
+My=86
+
+Mz=50
 
 INNAME=pism_Altesch_1880.nc
 
-mpirun -np 4 pismr -config_override aletsch_config.nc -boot_file $INNAME -Mx $Mx -My $My -Mz $Mx -Lz 2000 -sia_flow_law isothermal_glen -no_energy -surface elevation -ice_surface_temp -5,-25,0,4000 -climatic_mass_balance -2,2,0,2700,5400 -o_size big -y 10
+mpirun -np 4 pismr -config_override aletsch_config.nc -boot_file $INNAME -Mx $Mx -My $My -Mz $Mx -Lz 1000 -sia_flow_law isothermal_glen -no_energy -surface elevation -ice_surface_temp -5,-25,0,4000 -climatic_mass_balance -1.55,1.1,0,2800,4800 -o_order zyx -o_size big -y 100
