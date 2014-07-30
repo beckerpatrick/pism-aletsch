efcabbf291f7bd72bf00fe5efd0d1879914da382
Patrick Becker <becker@vaw.baug.ethz.ch>
2014-07-30 12:15:23 +0200

    nun läuft das script. so sollte es stimmen.

--- PDD/forcing_climatedata4x4.py
+++ PDD/forcing_climatedata4x4.py
@@ -23,14 +23,16 @@ from netcdftime import utime
 
 # Parameter festlegen
 start_date = parse('01.01.1865')
-end_date = parse('01.01.2009')
+end_date = parse('01.01.2008')
 ref_date = parse('01.01.1864')
 ref_unit = 'days'
-prule = rrule.YEARLY # AUCHTUNG ZUM TEST YEARLY
-#prule = rrule.DAILY
+prule = rrule.DAILY
 netcdfFlavor = 'NETCDF3_CLASSIC'
 
 outname = '4x4climateforcing.nc'
+
+zeitspanne =(end_date.year - start_date.year)*365 + end_date.day - start_date.day
+print('zeitspanne ' + str(zeitspanne))
 
 # Klimadaten erfassen
 hoeheDerWetterstation = 2766 # in Meter
@@ -51,23 +53,27 @@ prec_row = 4
 anzahlTage = climatedata.shape[0]
 
 # Klimadaten aufbereiten
+print('converting climate data...')
 temp_data = climatedata[:,temp_row]
 prec_data = climatedata[:,prec_row]
 prec_data = precipitation_conversion(prec_data)
 
-
 # Aufbau NetCDF-Datei
+print('setup netCDF...')
 nc = NC(outname, 'w', format=netcdfFlavor)
 
+
 # create a new dimension for bounds only if it does not yet exist
+print('creating time dimension...')
 time_dim = "time"
 if time_dim not in nc.dimensions.keys():
     nc.createDimension(time_dim)
 
 
 # x und y erzeugen
-easting = np.arange(634000, 651500, 1)
-northing = np.arange(123600, 157500, 1)
+print('creating spatial dimensions...')
+easting = np.arange(634000, 651500, 8750) # 2x2-Netz aufspannen (8750 *2 = 651500-634000)
+northing = np.arange(123600, 157500, 16950) # 2x2-Netz aufspannen (16950 *2 = 157500-123600)
 fill_value = -9999
 x_dim = 'x'
 if x_dim not in nc.dimensions.keys():
@@ -117,6 +123,7 @@ bnds_var_name = "time_bnds"
 # calculate the days since refdate, including refdate, with time being the
 # mid-point value:
 # time[n] = (bnds[n] + bnds[n+1]) / 2
+print('calculateing the days since refdate...')
 bnds_interval_since_refdate = cdftime.date2num(bnds_datelist)
 my_rule = rrule.DAILY
 datelist = list(rrule.rrule(my_rule, dtstart=start_date, until=end_date))
@@ -142,28 +149,34 @@ time_bnds_var[:,0] = bnds_interval_since_refdate[0:-1]
 time_bnds_var[:,1] = bnds_interval_since_refdate[1::]
 
 # check ob NC funktioniert
+print('check if NC works...')
 print nc.data_model
 
+print('create air temp variable...')
 temp_var = nc.createVariable('air_temp', 'f',dimensions=(time_dim, y_dim, x_dim),fill_value=fill_value)
 temp_var.long_name = 'air temp 2m above the surface'
 temp_var.units = 'degC'
 temp_var.comment = '''test'''
 
+print('create prec variable...')
 prec_var = nc.createVariable('precipitation', 'f',dimensions=(time_dim, y_dim, x_dim),fill_value=fill_value)
 prec_var.long_name = 'precipitation'
 prec_var.units = 'kg m-2 year-1'
 prec_var.comment = '''test'''
 
+print('create surf elevation variable...')
 prec_var = nc.createVariable('surface_elevation', 'f',dimensions=(y_dim, x_dim),fill_value=fill_value)
 prec_var.long_name = 'surface_elevation of the wheater station'
 prec_var.units = 'm'
 prec_var.comment = '''elevation field whith surface elevation of the wheater station in every grid cell'''
 
+print('create temp_lapse_rate variable...')
 prec_var = nc.createVariable('temp_lapse_rate', 'f',dimensions=(),fill_value=tempGradient)
 prec_var.long_name = 'temperature lapse rate'
 prec_var.units = 'degC m-1'
 prec_var.comment = '''no comment'''
 
+print('create prec_lapse_rate variable...')
 prec_var = nc.createVariable('precip_lapse_rate', 'f',dimensions=(),fill_value=niederschlagGradient)
 prec_var.long_name = 'precipitation lapse rate'
 prec_var.units = 'm-1'
@@ -174,9 +187,10 @@ prec_var.comment = '''no comment'''
 start_day = 0
 end_day = anzahlTage
 
-for day in range(start_day,3): #ACHTUNG!!! ZUM TEST nur 10 Tage statt 52000 (end_day)
-#~ for day in range(start_day,end_day):
-    print("processing record {} of {}".format(day, end_day))
+print('create records...')
+#for day in range(start_day,end_day-365): #ACHTUNG!!! ZUM TEST nur 10 Tage statt 53000 (end_day)
+for day in range(0,zeitspanne):
+    print("processing record {} of {}".format(day, zeitspanne))
     nc.variables['air_temp'][day,:] = temp_data[day]
     nc.variables['precipitation'][day,:] =prec_data[day]
 
