6e3f2dae062c7b5cb76d64cf26c2dff8edbd8049
Andy Aschwanden <aaschwanden@alaska.edu>
2014-09-12 09:47:48 -0800

    some fixes need to run scripts

--- preprocess.sh
+++ preprocess.sh
@@ -9,7 +9,7 @@ if [ $# -gt 0 ] ; then
 fi
 
 srs="epsg:21781"
-python create_aletsch_1880.py
+python create_aletsch/create_aletsch_1880.py
 # nc2cdo.py from pism/util/ is your friend:
 # it can calculate lon/lat and bounds based on the mapping variable, the global
 # attribute 'projection' and as a command line option --srs. 
@@ -17,7 +17,7 @@ python create_aletsch_1880.py
 nc2cdo.py --srs $srs pism_Aletsch_1880.nc
 # im nachfolgenden Schritt wird die Aufloesung von pism_aletsch_2009(!) auf 50m reduziert
 gdalwarp -overwrite -s_srs epsg:21781 -te 633975 123575 651525 157525 -t_srs epsg:21781 -tr 50 50 -r bilinear data/aletsch_surface_2009.grid data/aletsch_surface_2009_50m.grid
-python create_aletsch_2009.py
+python create_aletsch/create_aletsch_2009.py
 nc2cdo.py --srs $srs pism_Aletsch_2009.nc
 
 cmb_prefix=aletsch_cmb_1865-2008
